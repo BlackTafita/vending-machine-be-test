11ced637598111ac53a5a68f9809d180ff2a6d93
Nikita Reutov <liljollier@gmail.com>
2023-11-17 20:56:22 +0200

    feat(app): connect config module, add databse provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CoreModule } from './core/core.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
